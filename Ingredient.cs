1a02b8ce0cf69db02f72047becf7090213cecfbf
Kurban <idrissov09kub@gmail.com>
2026-03-08 02:07:32 +0700

    16

--- Ingredient.cs
+++ Ingredient.cs
@@ -2,8 +2,7 @@ namespace KurbanChef
 {
     public class Ingredient : BaseProduct //!!!!!
     {
-        private decimal _price;
-        public override decimal Price => _price; //!!!!!
+
         public Ingredient (string name, decimal price) : base(name)
         {
             Price = price;
--- PizzaBase.cs
+++ PizzaBase.cs
@@ -3,17 +3,15 @@ namespace KurbanChef
     public class PizzaBase : BaseProduct
     {
         public bool IsClassic { get; set; }
-        private decimal _basePrice;
-
-        public override decimal Price => _basePrice;
 
         public PizzaBase(string name, decimal price, bool isClassic, decimal classicPrice = 0) : base(name)
         {
             IsClassic = isClassic;
-            // тут инкапусляция
+
             if (!isClassic && classicPrice > 0 && price > classicPrice * 1.2m)
             {
-                _basePrice = classicPrice * 1.2m;
+                Price = classicPrice * 1.2m;
+                Console.WriteLine($"Внимание: цена '{name}' была снижена до {Price}");
             }
             else
             {
